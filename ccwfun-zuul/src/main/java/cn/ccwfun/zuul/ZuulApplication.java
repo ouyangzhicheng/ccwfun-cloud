11ccf0fcc2583ca14e5c54db081e4994bcb97871
Along <296729445@qq.com>
2019-03-28 19:29:03 +0800

    create ccwfun-zuul

---
+++ ccwfun-zuul/src/main/java/cn/ccwfun/zuul/ZuulApplication.java
@@ -0,0 +1,14 @@
+package cn.ccwfun.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+        System.out.print("ZuulApplication Success-------------------------->");
+    }
+}
